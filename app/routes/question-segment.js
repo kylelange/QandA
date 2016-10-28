729313f894208cd6c60e49577e631a843a38f97e
Kyle Lange <baronsintrees@gmail.com>
2016-10-28 15:40:53 -0700

    erase all child answers attached to a question when the question is deleted. v1.3.

--- app/routes/question-segment.js
+++ app/routes/question-segment.js
@@ -6,7 +6,12 @@ export default Ember.Route.extend({
   },
   actions: {
     destroyQuest(question) {
-      question.destroyRecord();
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      });
       this.transitionTo('index');
     },
 
