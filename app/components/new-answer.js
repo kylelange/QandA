e9219f8f2baa5713e298f9f686145b06159e84fa
Kyle Lange <baronsintrees@gmail.com>
2016-10-28 11:59:28 -0700

    add working add answer form to the index template.  Next: move it to each dynamic segment.

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAns: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAns', true);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        ans: this.get('ans')
+      };
+      this.set('addNewAns', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
