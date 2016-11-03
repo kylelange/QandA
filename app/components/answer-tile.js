905618931fd4577016765078566944b4e2b636a3
Kyle Lange <baronsintrees@gmail.com>
2016-11-03 09:19:36 -0700

    add a simple computed property to the answer display on the answer-tile.  The comp. property is called computedAnswer.

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  computedAnswer: Ember.computed('answer.author', 'answer.ans', function() {
+    return this.get('answer.author') + ' says: ' + this.get('answer.ans');
+  }),
+
   actions: {
     delete(answer) {
       if (confirm('Are you sure you want to delete this answer?')) {
